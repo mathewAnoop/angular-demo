58ded321d5a91e4d9e65dd91c9d4966f5efc6de5
mathewAnoop <mathewanoop1998@gmail.com>
2023-11-10 17:20:51 +0530

    newproject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'be-angular-app';
+  subtitle = 'Angular Version 17';
+  intro = 'Angularis a development platform and application design framework for building smart and effective single-page applications. Google is the company behind Angular.'
+  feature = 'Built-in control flow: To improve the developer experience, Angular 17 has released a new block template syntax that gives you powerful features with simple, declarative APIs. Under the hood, the Angular compiler transforms the syntax into efficient JavaScript instructions that could perform control flow, lazy loading, and more.;'
 }
